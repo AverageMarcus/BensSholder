1bc902fe9400c5fcff9ee096bfcd3840e68e97fc
Marcus Noble <github@marcusnoble.co.uk>
2016-04-24 20:50:39 +0100

    Added index

--- app.js
+++ app.js
@@ -10,29 +10,37 @@ server.connection({
   port: process.env.PORT || 5000
 });
 
-server.route([
-  {
-    method: 'GET',
-    path:'/',
-    handler: function (req, reply) {
-      return reply(``);
-    }
-  },
-  {
-    method: 'GET',
-    path:'/{x}/{y}',
-    handler: function (req, reply) {
+server.register(require('inert'), (err) => {
 
-      imageHelper.getImage(req.params.x, req.params.y)
-        .then((buffer) => {
-          return reply(buffer).type('image/png').header('Access-Control-Allow-Origin', '*');
-        })
-        .catch(() => {
-          return reply('Unable to get image').code(404);
-        });
-    }
+  if (err) {
+      throw err;
   }
-]);
+
+  server.route([
+    {
+      method: 'GET',
+      path:'/{file}',
+      handler: function (req, reply) {
+        reply.file(`public/${req.params.file}`);
+      }
+    },
+    {
+      method: 'GET',
+      path:'/{x}/{y}',
+      handler: function (req, reply) {
+
+        imageHelper.getImage(req.params.x, req.params.y)
+          .then((buffer) => {
+            return reply(buffer).type('image/png').header('Access-Control-Allow-Origin', '*');
+          })
+          .catch(() => {
+            return reply('Unable to get image').code(404);
+          });
+      }
+    }
+  ]);
+
+});
 
 server.start((err) => {
   if (err) {
